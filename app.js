b38dc308ba94d6e3159b74e66dce05d13a6b4674
Kamil Nowocin <kam.nowocin@gmail.com>
2020-09-13 21:27:01 +0200

    feat: Game improvements

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
 //GLOBAL VARIABLES//
-let userScore = 0;
-let computerScore = 0;
+let playerScore = 0
+let computerScore = 0
+let roundsPlayed = 0
 
 //DOM ELEMENTS//
-const HTML_startGameButton = document.getElementById('startGame')
-const HTML_mainScreen = document.getElementById('welcomeScreen')
+const HTML_startGameButton = document.querySelector('.welcomeScreen > button')
+const HTML_welcomeScreen = document.querySelector('.welcomeScreen')
 const HTML_gameArena = document.querySelector('.gameArena')
-const HTML_userScore = document.getElementById('userScore');
-const HTML_computerScore = document.getElementById('computerScore');
-const HTML_gameResult = document.querySelector('.gameResult > h1');
-const HTML_restartGame = document.getElementById('restartGame')
+const HTML_userScore = document.getElementById('userScore')
+const HTML_computerScore = document.getElementById('computerScore')
+const HTML_gameResult = document.querySelector('.gameResult > h1')
+const HTML_restartGameButton = document.querySelector('.restartGame > button')
+const HTML_roundsDigit = document.getElementById('roundsDigit')
+const HTML_roundsNumberText = document.querySelector('.roundsNumber > p')
+const HTML_roundsCounter = document.getElementById('roundsCounter')
+const HTML_endGameScreen = document.querySelector('.endGame')
+const HTML_endUserScore = document.getElementById('userEndGameScore')
+const HTML_endComputerScore = document.getElementById('computerEndGameScore')
 
 //GAME SYMBOLS//
-const rockSymbol_HTML = document.getElementById('rock');
-const scissorsSymbol_HTML = document.getElementById('scissors');
-const paperSymbol_HTML = document.getElementById('paper');
+const HTML_rockSymbol = document.getElementById('rock')
+const HTML_scissorsSymbol = document.getElementById('scissors')
+const HTML_paperSymbol = document.getElementById('paper')
+
+//LISTENERS
+HTML_restartGameButton.addEventListener('click', () => clearResult())
+HTML_rockSymbol.addEventListener('click', () => calculateGameResult('rock'))
+HTML_scissorsSymbol.addEventListener('click', () => calculateGameResult('scissors'))
+HTML_paperSymbol.addEventListener('click', () => calculateGameResult('paper'))
+
+startGame()
 
 function startGame() {
     HTML_startGameButton.addEventListener(('click'), () => {
-        playSound('start')
-        HTML_mainScreen.classList.add('fadeOut')
-        HTML_gameArena.className = 'gameArena fadeIn'
+        checkNumberOfRounds(HTML_roundsDigit.value)
+        window.scrollTo(0, 0);
     })
-    getPlayerSymbol()
 }
 
-function getPlayerSymbol() {
-    rockSymbol_HTML.addEventListener('click', () => calculateGameResult('rock'))
-    scissorsSymbol_HTML.addEventListener('click', () => calculateGameResult('scissors'))
-    paperSymbol_HTML.addEventListener('click', () => calculateGameResult('paper'))
+function checkNumberOfRounds(playerInput) {
+    if (isNaN(playerInput) || playerInput <= 0) {
+        HTML_roundsNumberText.innerHTML = `This is not valid number of rounds! ${playerInput}`
+        symbolBorderGlow('roundsDigit', 'red-glow')
+        HTML_roundsDigit.focus()
+        HTML_roundsDigit.value = ''
+    } else {
+        playSound('start')
+        HTML_welcomeScreen.classList.add('fadeOut')
+        HTML_gameArena.className = 'gameArena fadeIn'
+    }
 }
 
 function calculateGameResult(playerChosenSymbol) {
@@ -67,55 +87,60 @@ function getComputerSymbol() {
     }
 }
 
-function symbolBorderGlow(elementID, cssClass) {
-    document.getElementById(elementID).classList.add(cssClass)
-    setTimeout(() => document.getElementById(elementID).classList.remove(cssClass), 500)
-}
-
 function getWinner(playerChosenSymbol, computerChosenSymbol, winner) {
+    HTML_roundsCounter.innerHTML = `Round ${roundsPlayed + 1} of ${HTML_roundsDigit.value}`
     switch (winner.toLowerCase()) {
         case 'draw':
+            endGame(computerScore, playerScore)
+            roundsPlayed++
             playSound('draw')
-            HTML_gameResult.innerHTML = 'It\'s a draw!'
+            HTML_gameResult.innerHTML = 'It\'s a draw 💁🏻‍♂️'
             symbolBorderGlow(computerChosenSymbol, 'white-glow')
             symbolBorderGlow(playerChosenSymbol, 'white-glow')
             break
         case 'computer':
             playSound('lose')
             computerScore++
+            endGame(computerScore, playerScore)
+            roundsPlayed++
             HTML_computerScore.innerHTML = computerScore.toString()
-            HTML_userScore.innerHTML = userScore.toString()
+            HTML_userScore.innerHTML = playerScore.toString()
             HTML_gameResult.innerHTML = `${computerChosenSymbol.charAt(0).toUpperCase()}${computerChosenSymbol.slice(1)}
-             beats ${playerChosenSymbol.charAt(0).toUpperCase()}${playerChosenSymbol.slice(1)}, You lost 🤖`
+             beats ${playerChosenSymbol.charAt(0).toUpperCase()}${playerChosenSymbol.slice(1)}, You lost 🤦🏻‍♂️`
             symbolBorderGlow(computerChosenSymbol, 'green-glow')
             symbolBorderGlow(playerChosenSymbol, 'red-glow')
             break
         case 'player':
             playSound('win')
-            userScore++
-            HTML_userScore.innerHTML = userScore.toString()
+            playerScore++
+            endGame(computerScore, playerScore)
+            roundsPlayed++
+            HTML_userScore.innerHTML = playerScore.toString()
             HTML_computerScore.innerHTML = computerScore.toString()
             HTML_gameResult.innerHTML = `${playerChosenSymbol.charAt(0).toUpperCase()}${playerChosenSymbol.slice(1)}
-             beats ${computerChosenSymbol.charAt(0).toUpperCase()}${computerChosenSymbol.slice(1)}, You win 👦🏻`
+             beats ${computerChosenSymbol.charAt(0).toUpperCase()}${computerChosenSymbol.slice(1)}, You win 🙋🏻‍♂️`
             symbolBorderGlow(playerChosenSymbol, 'green-glow')
             symbolBorderGlow(computerChosenSymbol, 'red-glow')
             break
-
         default:
             throw new Error(`Unknown step: ${winner}`)
     }
 }
 
-HTML_restartGame.addEventListener('click', clearResult)
-
-function clearResult() {
-    HTML_restartGame.addEventListener(('click'), () => {
-        userScore = 0;
-        computerScore = 0;
-        HTML_gameResult.innerHTML = 'Chose your symbol'
-        HTML_userScore.innerHTML = userScore.toString();
-        HTML_computerScore.innerHTML = computerScore.toString()
-    })
+function endGame(computerScore, playerScore) {
+    if (parseInt(HTML_roundsDigit.value) === roundsPlayed + 1) {
+        HTML_endUserScore.innerHTML = playerScore
+        HTML_endComputerScore.innerHTML = computerScore
+        HTML_gameArena.className = 'gameArena fadeOut'
+        HTML_endGameScreen.className = 'endGame fadeIn'
+        if (computerScore > playerScore) {
+            console.log("AI win!")
+        } else if (computerScore < playerScore) {
+            console.log("User win!")
+        } else if (computerScore === playerScore) {
+            console.log("Draw!")
+        }
+    }
 }
 
 function playSound(soundEffect) {
@@ -130,6 +155,7 @@ function playSound(soundEffect) {
             new Audio('assets/media/draw.wav').play()
             break
         case 'start':
+        case 'restart':
             new Audio('assets/media/start.wav').play()
             break
         default:
@@ -137,4 +163,18 @@ function playSound(soundEffect) {
     }
 }
 
-startGame()
+function symbolBorderGlow(elementID, cssClass) {
+    document.getElementById(elementID).classList.add(cssClass)
+    setTimeout(() => document.getElementById(elementID).classList.remove(cssClass), 500)
+}
+
+function clearResult() {
+    playSound('restart')
+    playerScore = 0
+    computerScore = 0
+    roundsPlayed = 0
+    HTML_gameResult.innerHTML = 'Chose your symbol'
+    HTML_roundsCounter.innerHTML = `Let the game begin`
+    HTML_userScore.innerHTML = playerScore.toString()
+    HTML_computerScore.innerHTML = computerScore.toString()
+}
